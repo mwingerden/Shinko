356b066c8bb1b00a2595d5e34d281e1467d60c04
Matthew Wingerden <matthew.wingerden@gmail.com>
2024-07-13 20:48:31 -0700

    Added a new turned based combat logic. Still working on it.

--- Scripts/Enemies/enemy.gd
+++ Scripts/Enemies/enemy.gd
@@ -1,28 +1,25 @@
 extends CharacterBody2D
 
+@onready var selected = $Selected
+@onready var progress_bar = $Health
+@onready var animation_player = $AnimationPlayer
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var MAX_HEALTH = 5
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+var current_health = MAX_HEALTH:
+	set(value):
+		current_health = value
+		_update_progress_bar()
+		_play_animation()
+		
+func _update_progress_bar():
+	progress_bar.value = (current_health/MAX_HEALTH) * 100
+	
+func _play_animation():
+	pass
+	
+func _selected():
+	selected.show()
+	
+func _unselect():
+	selected.hide()
