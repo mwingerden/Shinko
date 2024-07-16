d9f95e6ee3440021d2d3396714c9fdbecc5a7309
mwingerden <matthew.wingerden@gmail.com>
2024-07-16 16:13:35 -0700

    Finished a simple turned based combat scenario.

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,4 +1,18 @@
 extends CharacterBody2D
 
-func play_turn():
-	print("Player Turn.")
+@onready var progress_bar = $Health
+@export var MAX_HEALTH = 20
+var current_health = MAX_HEALTH:
+	set(value):
+		current_health = value
+		_update_progress_bar()
+		_play_animation()
+		
+func _update_progress_bar():
+	progress_bar.value = (float(current_health) / MAX_HEALTH) * 100
+	
+func _play_animation():
+	pass
+
+func take_damage(value):
+	current_health -= value
