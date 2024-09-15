999e0bc7ae750aba08513b300510390f5d1620c1
Matthew Wingerden <matthew.wingerden@gmail.com>
2024-09-14 18:15:26 -0700

    added loop to the game music

--- Scripts/Music/audio_player.gd
+++ Scripts/Music/audio_player.gd
@@ -12,3 +12,12 @@ func _player_music(music: AudioStream, volume = 0.0):
 
 func play_music_level():
 	_player_music(level_music)
+	
+func play_FX(stream: AudioStream, volume = 0.0):
+	var fx_player = AudioStreamPlayer.new()
+	fx_player.stream = stream
+	fx_player.name = "FX_PLAYER"
+	fx_player.volume_db = volume
+	add_child(fx_player)
+	await fx_player.finished
+	fx_player.queue_free()
